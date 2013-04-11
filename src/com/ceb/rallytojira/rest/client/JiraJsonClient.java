dc42c9503d03d7aee5a09b5d4c5f9842b6e65b7a
hagarwal <hagarwal@DX7QGT1.ceb.com>
2013-04-11 16:55:10 -0400

    1

--- src/com/ceb/rallytojira/rest/client/JiraJsonClient.java
+++ src/com/ceb/rallytojira/rest/client/JiraJsonClient.java
@@ -8,7 +8,7 @@ import com.ceb.rallytojira.rest.api.JiraRestApi;
 public class JiraJsonClient {
 
 	public JiraRestApi getJiraRestApi() throws URISyntaxException {
-		return new JiraRestApi(new URI("http://agiletool.executiveboard.com"),
+		return new JiraRestApi(new URI("http://206.16.231.96"),
 				"rally_jira_migration", "rally_jira_migration");
 	}
 }
